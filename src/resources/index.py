abaffe17b7953e3d20f930747412f12c8243d634
klaus-moser <don_kanaille@posteo.de>
2021-04-02 22:58:36 +0200

    Register WTForms

--- src/resources/index.py
+++ src/resources/index.py
@@ -8,20 +8,49 @@ class Index(Resource):
     """
     Home Resource.
     """
-    def get(self) -> tuple:
+    def __init__(self):
+        self.headers = {'Content-Type': 'text/html'}
+
+    def get(self):
         """
         Render the index.html page.
 
         :return: index.html
         """
         index_form = IndexForm()
-
-        headers = {'Content-Type': 'text/html'}
-        return make_response(render_template('index.html', form=index_form), 200, headers)
+        return make_response(render_template('index.html', form=index_form), 200, self.headers)
 
     def post(self):
+        """
+        Processes the 4 different submit-button.
 
-        contact_form = ContactForm()
+        :return: Renders one of the pages /login, /register, /contact or /gallery.
+        """
+        # Import WTForms
+        index = IndexForm()
+        # login_form = LoginForm()  # TODO
+        # register_form = RegisterForm()  # TODO
+        contact_form = ContactForm()  # TODO
+        # gallery_form = GalleryForm()  # TODO
 
-        headers = {'Content-Type': 'text/html'}
-        return make_response(render_template('contact.html', form=contact_form), 200, headers)
+        if index.validate_on_submit():
+            # Login
+            if index.login_button.data:
+                pass
+                # return make_response(render_template('login.html', form=contact_form), 200, self.headers)
+
+            # Register
+            elif index.register_button.data:
+                pass
+                # return make_response(render_template('register.html', form=contact_form), 200, self.headers)
+
+            # Contact
+            elif index.contact_button.data:
+                return make_response(render_template('contact.html', form=contact_form), 200, self.headers)
+
+            # View
+            elif index.overview_button.data:
+                pass
+                # return make_response(render_template('gallery.html', form=contact_form), 200, self.headers)
+
+        return redirect(url_for('index'))  # TODO: necessary?
--- src/wtform_fields.py
+++ src/wtform_fields.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
 from passlib.hash import pbkdf2_sha256
 
@@ -21,13 +21,47 @@ class ContactForm(FlaskForm):
     This is the form class for the '/contact' endpoint.
     """
     # TODO: check existing email (registered user)
-    # TODO: check email format
+    # TODO: check email format (Regex)
     email_field = StringField('email_field', validators=[
         InputRequired(message='Email required!'),
-        Length(min=4, max=25, message='Put in a valid E-Mail adress!')])
+        Length(min=4, max=25, message='Put in a valid E-Mail address!')])
 
-    text_field = StringField('text_field', validators=[
+    text_field = TextAreaField('text_field', validators=[
         InputRequired(message='Message'),
-        Length(min=4, max=25, message='Message cannot be empty!')])
+        Length(min=1, max=200, message='Message cannot be empty!')])
 
     send_button = SubmitField('Send')
+
+
+class RegisterForm(FlaskForm):
+    """
+    This is the form class for the '/register' endpoint.
+    """
+    username = StringField('username_label', validators=[
+        InputRequired(message='Username required!'),
+        Length(min=4, max=25, message='Username must be between 4-25 characters!')])
+
+    email = StringField('email_label', validators=[
+        InputRequired(message='Email required!'),
+        Length(min=4, max=25, message='Must be a valid Email!')])
+
+    password = PasswordField('password_label', validators=[
+        InputRequired(message='Password required!'),
+        Length(min=4, max=25, message='Password must be between 4-25 characters!')])
+
+    confirm_pwd = PasswordField('confirm_pwd_label', validators=[
+        InputRequired(message='Password required!'),
+        EqualTo('password', message='Passwords must match!')])
+
+    submit_button = SubmitField('Register')
+
+    # Custom validator to check username upfront
+    def validate_username(self, username) -> None:
+        """
+        Validate a given username.
+
+        :param username: Username to be validated via the database.
+        :return: None.
+        """
+        if UserModel.find_by_username(username=username.data):
+            raise ValidationError("A user '{}' already exists!".format(username.data))
