6779095de3e5b7c1bfcf4bf992f49c526bea49dd
klaus-moser <don_kanaille@posteo.de>
2021-04-04 22:12:57 +0200

    index.html

--- src/resources/main.py
+++ src/resources/main.py
@@ -14,7 +14,7 @@ class Index(Resource):
         """
         if current_user.is_authenticated:
             return redirect(url_for('gallery'))
-        return make_response(render_template('main/index.html'), 200)
+        return make_response(render_template('main/index.html', title="Index"), 200)
 
 
 class Gallery(Resource):
@@ -26,9 +26,32 @@ class Gallery(Resource):
         """
         Render the 'gallery.html' page.
         """
-        if current_user.is_authenticated:
-            return "Hello World"
-        else:
-            return "Not Hello World"
+        return make_response(render_template('main/gallery.html', title="Gallery"), 200)
 
 
+class Contact(Resource):
+    """
+    Contact Resource.
+    """
+    def __init__(self):
+        self.headers = {'Content-Type': 'text/html'}
+
+    def get(self):
+        """
+        Render the 'contact.html'.
+
+        :return: 'contact.html'
+        """
+        contact_form = ContactForm()
+        return make_response(render_template('main/contact.html', form=contact_form), 200, self.headers)
+
+    def post(self):
+        """
+        Takes the post request to send the E-mail.
+        Pop-Up to inform the user.
+
+        :return: '/'
+        """
+        # TODO: send email
+        # TODO: Info-pop up
+        return redirect('/')
