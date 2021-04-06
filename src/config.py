3dbcec8b1368873538b9f346646c872a8e4b6dc8
klaus-moser <don_kanaille@posteo.de>
2021-04-06 17:07:09 +0200

    Removed obsolete code

--- src/config.py
+++ src/config.py
@@ -32,7 +32,7 @@ class Config(object):
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
     JWT_TOKEN_LOCATION = ['cookies']
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(seconds=120)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(seconds=3600)
     JWT_COOKIE_SECURE = False
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=15)
     JWT_COOKIE_CSRF_PROTECT = True
